d3f2ff6be2ffac743af5c2859a0735dea93b23bf
Yogesh Shah <yogeshshah2063@gmail.com>
2025-01-11 10:45:56 -0500

    Connect db with organization gmail:

--- Paw-Laya-Server/DB/db.js
+++ Paw-Laya-Server/DB/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectDB = async (req, res) => {
+  //DB USERNAME = teampawlaya
+  //DB PASSWORD = DM2KzKueFff3JXvu
   try {
     const connectionUri = process.env.DB_URL;
     mongoose.connect(connectionUri);
